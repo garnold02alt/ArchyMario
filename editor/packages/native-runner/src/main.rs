7bd2892e6e21dc362987e6c79d674d3af533071c
garnold02 <gulyas.arnold02@protonmail.com>
2022-03-31 22:15:20 +0200

    Loading all the assets

--- editor/packages/native-runner/src/main.rs
+++ editor/packages/native-runner/src/main.rs
@@ -1,13 +1,16 @@
 mod comms;
+mod repo;
 
 use std::{
-    fs,
+    fs::{self, File},
+    io::BufReader,
     sync::mpsc::channel,
     time::{SystemTime, UNIX_EPOCH},
 };
 
-use app::{builtin_resources, run, FromHost, Host, Init, ToHost, Winit};
+use app::{builtin_resources, run, FromHost, Host, Init, Resource, ResourceKind, ToHost, Winit};
 use comms::AsyncStdin;
+use repo::Repo;
 use winit::{event_loop::EventLoop, window::WindowBuilder};
 
 fn main() {
@@ -15,6 +18,45 @@ fn main() {
 
     for resource in builtin_resources() {
         sender.send(FromHost::LoadResource(resource)).unwrap();
+    }
+
+    {
+        let repo: Repo = serde_json::from_reader(BufReader::new(
+            File::open("../frontend/public/assets/repo.json").unwrap(),
+        ))
+        .unwrap();
+
+        for texture in repo.textures {
+            let buf = fs::read(format!(
+                "../frontend/public/assets/textures/{}.png",
+                texture.name
+            ))
+            .unwrap();
+
+            sender
+                .send(FromHost::LoadResource(Resource {
+                    id: texture.id,
+                    buf,
+                    kind: ResourceKind::Texture,
+                }))
+                .unwrap();
+        }
+
+        for prop in repo.props {
+            let buf = fs::read(format!(
+                "../frontend/public/assets/props/{}.amdl",
+                prop.name
+            ))
+            .unwrap();
+
+            sender
+                .send(FromHost::LoadResource(Resource {
+                    id: prop.id,
+                    buf,
+                    kind: ResourceKind::Prop,
+                }))
+                .unwrap();
+        }
     }
 
     let _stdin = AsyncStdin::new(sender);
